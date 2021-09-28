6c9beb1719b36705b68c5bf0887d116de2319a69
KevinRoblesGamboa <kevinroblesgambo@gmail.com>
2021-09-28 17:02:10 -0600

    Trabajo de semana 3

---
+++ Semana3/src/main/java/com/sc202/semana3/Practica1.java
@@ -0,0 +1,16 @@
+
+package com.sc202.semana3;
+
+public class Practica1 {
+    public static void main (String [] args)
+    {
+   /* for(int i = 0; i<7; i++)
+    {
+    System.out.println("Hola estoy siendo ejecutado desde un for");
+    */ 
+    for(int i = 0; i < 5; i++){
+    System.out.println("@@@@@@@@@@");
+  }
+}
+}
+

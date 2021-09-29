97201cbda9611e1de41464b3541168e7585433ed
WillCT22 <wcalvotrejos@gmail.com>
2021-09-29 17:15:15 -0600

    Clase de la semana 3

---
+++ Clase#3/src/libary/Main.java
@@ -0,0 +1,26 @@
+package libary;
+
+public class Main {
+
+    public static void main(String[] args) {
+/*	for(int i =0; i !=8; i++){
+        System.out.println(i);
+    }
+    int j =7;
+    while(j!=1){
+        System.out.println("w");
+        j--;
+    }
+    }*/
+        for(int i =0;i<5;i++){
+            for(int k=0;k<10;k++){
+                if(k<9){
+                    System.out.print("@");
+                }else {
+                    System.out.println("@");
+                }
+            }
+
+        }
+    }
+}

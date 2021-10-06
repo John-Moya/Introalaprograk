4bfb60ebc6262bdbb6443880892dd6b4af92b546
melaniepicado <melaniepicado30@gmail.com>
2021-10-05 19:19:58 -0600

    "Semana 4"

---
+++ Semana 4/src/data/Casa.java
@@ -0,0 +1,13 @@
+package data;
+
+public class Casa {
+    // Características Materiales Lugar
+    String Características, Materiales, Lugar;
+
+    //Vivienda
+    public void vivienda(){
+        System.out.println("Da hogar a varias personas");
+    }
+
+
+}
---
+++ Semana 4/src/data/Tablets.java
@@ -0,0 +1,15 @@
+package data;
+
+public class Tablets {
+    //marca, modelo, año, caracteristicas
+    String marca, modelo, caracteristicas;
+    int año;
+
+    //crear docs, entretenimiento
+    public void docs(){
+        System.out.println("Creando documento");
+    }
+    public void entretenimiento(){
+        System.out.println("Reproduciendo serie");
+    }
+}

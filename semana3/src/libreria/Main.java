747fbd0fbcd55d45a63802a56b9852625b90b8de
kael-quiros <kaelquiros@gmail.com>
2021-10-12 18:13:52 -0600

    trabajo semana 3

---
+++ semana3/src/libreria/Main.java
@@ -0,0 +1,53 @@
+package libreria;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*for (int i = 0; i < 2; i++){
+            System.out.println("Hola estoy siendo ejecutado desde un for");
+
+        }*/
+        /*for (int i = 0; i <= 7; i++){
+            System.out.println("Kael Guapo");
+        }
+
+        int j = 0;
+        while (j < 5){
+            System.out.println("While");
+            j = j + 1;
+        }*/
+
+       /* int k = 0;
+        while (k <= 5){
+        System.out.println("kael bebe");
+        k = k + 1;
+
+    }*/
+
+        /*int k = 0;
+        do {
+            System.out.println("Do While");
+            k++;
+        } while (k<5);*/
+
+        /*for (int i = 0; i < 5; i++) {
+        System.out.println(i); // 0 1
+        for (int j = 0; j < 5; j++) {
+            System.out.println(j); //0 2
+        }
+    }*/
+////////PROBLEMA
+
+        /*for (int i = 0; i < 2; i++) {
+            System.out.println("@@@@@@@@@@"); // 0 1
+            for (int j = 0; j < 1; j++) {
+                System.out.println("@@@@@@@@@@"); //0 2
+            }
+        }*/
+
+
+
+
+
+    }
+}

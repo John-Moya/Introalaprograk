c5f5e8df8e43100cf87a6b8e028d340264a2a136
Kael-Quiros <kaelquiros@gmail.com>
2021-10-18 18:04:35 -0600

    tallercondi/

---
+++ tallercondi/src/libreria/Main.java
@@ -0,0 +1,35 @@
+package libreria;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/*int edad = 21;
+    if (edad > 17) System.out.println("Es mayor de edad");
+
+    int a = 5;
+    int b = 3;
+    if (a > b) System.out.println("El numero es A");
+    if (a != b)System.out.println("Los numeros son diferentes");
+    if (a == b) System.out.println("Los numeros son iguales");
+    if (a * 2 == a + 7) System.out.println("Los numeros son 5 t 3");
+    if ((a - 1)/2 == a / 2) */
+
+    /*int a = 5;
+    int b = 3;
+    if (a > b) {
+        System.out.println("El numero mayor es A");
+        System.out.println("El numero menor B");
+    }*/
+    int edad = 21;
+    if (edad < 18) System.out.println("El usuario es menor de edad");
+    if (edad >= 18) System.out.println("El usuario es mayor de edad");
+
+    if (edad >= 18) {
+        System.out.println("El usuario es mayor de edad");
+    }else {
+        System.out.println("El usuario es menor de edad");
+    }
+
+
+    }
+}
